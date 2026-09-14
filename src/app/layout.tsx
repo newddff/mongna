08ab4e8902ee8ce91835492e5183660ed8072938
pppoooni <jongwoop42@gmail.com>
2026-09-14 23:10:40 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+ icons: { 
+   icon: ' https://event.img.sooplive.com/note_image/2026/08/31/37806a95605eda196.png',
+ },
+};
 export default function RootLayout({
   children,
 }: {
